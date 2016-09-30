5f7243171b8a1d1305d440845e0c4826b6a34e57
Rui Zhao <ruizhao@Ruis-MacBook-Pro.local>
2016-09-29 23:34:00 -0500

    nothing

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3001;
+var todos = [
+    {
+    	id:1,
+    	description: "Good Morning",
+    	TORF: false
+    },
+    {
+    	id:2,
+    	description: "Good Afternoon",
+        TORF: false
+    },
+    {
+    	id:3,
+    	description: "Good Evening",
+    	TORF: true
+    }	
+];
+
+
+app.get("/todos",function(req,res){
+	res.json(todos);
+})
+
+app.get("/todos/:id",function(req,res){
+	var matchedID;
+	var todoID = parseInt(req.params.id);
+	todos.forEach(function(todo){
+		if (todoID === todo.id ){
+			matchedID = todo;
+		}
+	});
+		if(matchedID){
+			res.json(matchedID);
+		}else{
+            res.status(404).send();
+		}
+	
+});
 
 app.get('/',function(req,res){
 	res.send("Todo API Root");
